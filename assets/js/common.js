fc4eca3eed9a2103ecf2265b25dbb1252a962211
Hiroshi Fujiwara <fujiwara@no1s.biz>
2024-05-11 10:32:06 +0000

    commit

--- assets/js/common.js
+++ assets/js/common.js
@@ -83,3 +83,36 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const heads = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
+  if (heads && heads.length) {
+    let contents = "";
+    heads.forEach((head, i) => {
+      let className = "";
+      switch (head.localName) {
+        case "h1":
+          className = `contents1`;
+          break;
+        case "h2":
+          className = `contents2`;
+          break;
+        case "h3":
+          className = `contents3`;
+          break;
+        case "h4":
+          className = `contents4`;
+          break;
+        case "h5":
+          className = `contents5`;
+          break;
+        case "h6":
+          className = `contents6`;
+          break;
+      }
+      contents += `<li><a class="${className}" href="#head${i}">${head.textContent}</a></li>`;
+      head.innerHTML += `<a id="head${i}"></a>`;
+    });
+    document.querySelector("#pageNav").innerHTML += `<ol>${contents}</ol>`;
+  }
+});
